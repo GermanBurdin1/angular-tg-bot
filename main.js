b29a8f4f62b064380e3b329c8665e062ff42980c
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-07-18 15:36:27 +0200

    test toggle button

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import { Telegraf, Markup } from 'telegraf';
 const token = '7097254544:AAEsBL5MOHomKdELR-7yfAFyRooZy71-B_4';
+const webAppUrl = 'https://google.com';
+const bot = new Telegraf(token);
+
+bot.command('start',(ctx) => {
+	ctx.reply(
+		"Bienvenue ! Cliquez sur le bouton ci-dessous pour démarrer l'application.",
+		Markup.keyboard([
+			Markup.button.webApp(
+				'Envoyer le message',
+				webAppUrl
+			)
+		])
+	)
+})
+bot.launch();
